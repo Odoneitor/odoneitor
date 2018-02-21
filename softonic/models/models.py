e235f36703d463fdf402d8d8d4af86d04b158b56
Qazeke <sergiozgz18@gmail.com>
2018-02-21 08:43:34 +0100

    team added

--- softonic/models/models.py
+++ softonic/models/models.py
@@ -18,3 +18,14 @@ class Client(models.Model):
     #relatedProyects_ids = fields.one2Many(
     #   'softonic.proyect', 'client_id', string="Associated proyects"
     #)    
+
+class Team(models.Model):
+    _name     = 'softonic.team'
+    position  = fields.Char(string='Position', required=True, help='Programmer\'s position inside of the team...')
+    hours     = fields.Float(digits=(6,2), required=False, help='Assigned hours...')
+    # En el equipo debe haber una referencia al programador
+    #coder_id = fields.Many2one('softonic.coder',
+    #    ondelete='set null', string="Coder", index=True)
+    # En el equipo debe haber una referencia al proyecto
+    #proyect_id = fields.Many2one('softonic.proyect',
+    #    ondelete='set null', string="Proyect", index=True)    
